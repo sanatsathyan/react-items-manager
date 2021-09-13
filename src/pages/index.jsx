6623096c2415511bd7b175dd6e05bd1c45aad105
SANAT <sanat.sathyan@adek.gov.ae>
2021-09-14 01:56:36 +0530

    final push

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import {
   BrowserRouter as Router,
   Switch,
@@ -13,7 +13,9 @@ import { Layout } from "./layout";
 
 export const AppTest = () => {
   const dispatch = useDispatch();
-  const auth = localStorage.getItem("auth");
+  const userId = useSelector(
+    (state) => state.auth.session && state.auth.session.id
+  );
 
   useEffect(() => {
     initializeMockData();
@@ -36,7 +38,7 @@ export const AppTest = () => {
           </Route>
           <Route
             path="/"
-            render={(props) => (auth ? <Layout /> : <Redirect to="/login" />)}
+            render={(props) => (userId ? <Layout /> : <Redirect to="/login" />)}
           />
         </Switch>
       </Router>
